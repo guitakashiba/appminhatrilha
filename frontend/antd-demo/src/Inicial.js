8d205d88fd3a4fd95767900e3d891236c7751770
guitakashiba <takashiba30@gmail.com>
2023-05-04 19:08:22 -0300

    att tela Inicial

--- frontend/antd-demo/src/Inicial.js
+++ frontend/antd-demo/src/Inicial.js
@@ -1,91 +1,70 @@
-import React, { useState, useEffect } from "react";
-import { Checkbox } from "antd";
-import "./Inicial.css";
+import { useState, useEffect } from 'react';
+import { Card, List, Checkbox } from 'antd';
+import './Inicial.css';
+
 
 function TelaInicial() {
   const [disciplinas, setDisciplinas] = useState([]);
-  const [selectedDisciplinas, setSelectedDisciplinas] = useState([]);
-  const [cargaHorariaPorTipo, setCargaHorariaPorTipo] = useState({});
+  const [estadoCheckbox, setEstadoCheckbox] = useState({});
+  const [cargaHorariaTotal, setCargaHorariaTotal] = useState(0);
 
   useEffect(() => {
     fetch('http://localhost:3333/disciplinas')
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         setDisciplinas(data);
-
-        // Obtém a carga horária por tipo de disciplina
-        const cargaHorariaPorTipoObj = {};
-        data.forEach((disciplina) => {
-          const { tipo, cargaHoraria } = disciplina;
-          if (cargaHorariaPorTipoObj[tipo]) {
-            cargaHorariaPorTipoObj[tipo] += cargaHoraria;
-          } else {
-            cargaHorariaPorTipoObj[tipo] = cargaHoraria;
-          }
+        const estadoInicialCheckbox = {};
+        data.forEach(disciplina => {
+          estadoInicialCheckbox[disciplina.codigo] = false;
         });
-        setCargaHorariaPorTipo(cargaHorariaPorTipoObj);
+        setEstadoCheckbox(estadoInicialCheckbox);
       });
   }, []);
 
-  useEffect(() => {
-    // Obtém a carga horária selecionada pelo usuário
-    const cargaHorariaSelecionadaPorTipoObj = {};
-    selectedDisciplinas.forEach((disciplina) => {
-      const { tipo, cargaHoraria } = disciplina;
-      if (cargaHorariaSelecionadaPorTipoObj[tipo]) {
-        cargaHorariaSelecionadaPorTipoObj[tipo] += cargaHoraria;
-      } else {
-        cargaHorariaSelecionadaPorTipoObj[tipo] = cargaHoraria;
-      }
+  const handleCheckboxChange = (codigoDisciplina, checked, cargaHoraria) => {
+    setEstadoCheckbox({
+      ...estadoCheckbox,
+      [codigoDisciplina]: checked,
     });
-
-    // Atualiza a carga horária total por tipo de disciplina
-    setCargaHorariaPorTipo((prevState) => {
-      const newState = { ...prevState };
-      Object.entries(cargaHorariaSelecionadaPorTipoObj).forEach(([tipo, cargaHorariaSelecionada]) => {
-        newState[tipo] -= cargaHorariaSelecionada;
-      });
-      return newState;
-    });
-  }, [selectedDisciplinas]);
-
-  const handleCheckboxChange = (event) => {
-    const codigoDisciplina = event.target.value;
-    const disciplinaSelecionada = disciplinas.find((disciplina) => disciplina.codigo === codigoDisciplina);
-
-    // Adiciona ou remove a disciplina da lista de selecionadas
-    setSelectedDisciplinas((prevState) => {
-      if (prevState.includes(disciplinaSelecionada)) {
-        return prevState.filter((disciplina) => disciplina !== disciplinaSelecionada);
-      } else {
-        return [...prevState, disciplinaSelecionada];
-      }
-    });
+    if (checked) {
+      setCargaHorariaTotal(cargaHorariaTotal + cargaHoraria);
+    } else {
+      setCargaHorariaTotal(cargaHorariaTotal - cargaHoraria);
+    }
   };
-  
 
   return (
-    <div className="main-container">
-      <div className="disciplinas-container">
-        <h2 className="disciplinas-title">Lista de Disciplinas</h2>
-        <ul className="disciplinas-list">
-        {disciplinas.map((disciplina, index) => (
-            <li key={`${disciplina.codigo}-${index}`} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <Checkbox value={disciplina.codigo} onChange={handleCheckboxChange}>{`${disciplina.codigo} - ${disciplina.nome}`}</Checkbox>
-            </li>
-            ))}
-        </ul>
-      </div>
-      <div className="carga-horaria-container">
-        <h2 className="carga-horaria-title">Carga Horária</h2>
-        <ul className="carga-horaria-list">
-          {Object.entries(cargaHorariaPorTipo).map(([tipo, cargaHoraria]) => (
-            <li key={tipo}>{`${tipo}: ${cargaHoraria} horas`}</li>
-          ))}
-        </ul>
-      </div>
+    <div>
+      <Card title="Usuario" bordered={true} style={{ marginBottom: 16 }}>
+        {/* conteúdo do card */}
+      </Card>
+
+      <Card title="Lista de Disciplinas" bordered={true} style={{ marginBottom: 16 }}>
+        <List
+          itemLayout="horizontal"
+          dataSource={disciplinas}
+          renderItem={item => (
+            <List.Item>
+              <Checkbox
+                checked={estadoCheckbox[item.codigo]}
+                onChange={e => handleCheckboxChange(item.codigo, e.target.checked, item.cargaHoraria)}
+              />
+              <List.Item.Meta
+                title={item.codigo + " - " + item.nome}
+                ellipsis={true}
+              />
+            </List.Item>
+          )}
+          bordered={true}
+          style={{ overflow: 'scroll', maxHeight: '400px' }}
+        />
+      </Card>
+
+      <Card title="Carga Horária" bordered={true}>
+        <p>{cargaHorariaTotal} horas</p>
+      </Card>
     </div>
   );
 }
 
-export default TelaInicial
+export default TelaInicial;
