059e968d13e128df8dd9b0b6af33de3fbb0bd861
GRenkel <guirenkel@gmail.com>
2023-06-29 20:38:16 -0300

    feat: improvements on CSS

--- frontend/antd-demo/src/components/SobreNos.js
+++ frontend/antd-demo/src/components/SobreNos.js
@@ -1,8 +1,9 @@
 //SobreNos.js
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Image } from 'antd';
 //import { UserOutlined } from '@ant-design/icons';
 import { Carousel } from 'antd';
+import './sobreNos.css';
 const { Meta } = Card;
 
 const teamMembers = [
@@ -20,38 +21,34 @@ const supporters = [
 const projectOrigin = "Texto sobre a origem do projeto";
 
 const contentStyle = {
-  height: 'auto',
-  width: 'auto',
-  display: 'flex', // Adicionado para centralizar a imagem verticalmente
-  alignItems: 'center', // Adicionado para centralizar a imagem verticalmente
-  justifyContent: 'center', // Adicionado para centralizar a imagem horizontalmente
   color: '#fff',
   background: '#364d79',
 };
 
 const imageStyle = {
-  maxWidth: '100%', // A imagem vai tentar preencher o máximo de espaço horizontal disponível, mas não mais que isso
-  maxHeight: '100%', // A imagem vai tentar preencher o máximo de espaço vertical disponível, mas não mais que isso
-  objectFit: 'contain', // A imagem vai escalar proporcionalmente sem ser cortada
+  'background-size': 'contain', // A imagem vai escalar proporcionalmente sem ser cortada
 }
 
   function SobreNos() {
     return (
       <div className="site-card-wrapper">
-        <Card style={{ width: 'auto', height: 'auto' }}>
+        <Card 
+          style={{ width: '100%', maxHeight: 600}} 
+          bodyStyle={{width: '100%', maxWidth: '100%'}}
+        >
           <Carousel autoplay>
               {teamMembers.map((member, i) => (
-                  <div key={i}>
-                      <h3 style={contentStyle}>
-                        <img src={member.img} alt={`Membro ${i + 1}`} style={imageStyle}/>
-                        <p>{member.desc}</p>
-                      </h3>
-                  </div>
+                <div key={i} className='container'>
+                      <Image
+                      width={'500px'}
+                      src={member.img}
+                      />
+                </div>
               ))}
           </Carousel>
         </Card>
         <Card>
-          <Carousel autoplay>
+          <Carousel >
               {supporters.map((member, i) => (
                   <div key={i}>
                       <h3 style={contentStyle}>
