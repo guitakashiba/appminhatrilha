594181ce61b13cf30f3bf3f9796f88b1c5da0d11
guitakashiba <takashiba30@gmail.com>
2023-07-03 16:28:47 -0300

    fix: base_url cloud

--- frontend/antd-demo/src/services/api.js
+++ frontend/antd-demo/src/services/api.js
@@ -1,6 +1,6 @@
 
-const BASE_URL = 'http://localhost:3333'
-//const BASE_URL = 'http://44.201.185.184:3333'
+//const BASE_URL = 'http://localhost:3333'
+const BASE_URL = 'http://100.26.195.165:3333'
 
 export default {
   auth: {
